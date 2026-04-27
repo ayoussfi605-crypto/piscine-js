dab8e04e8072f01ed9d3faf3d651b7129e498aa1
aelyoussef <ayoubyouss4859@gmail.com>
2026-04-27 15:51:02 +0100

    more-or-less

---
+++ more-or-less.js
@@ -0,0 +1,27 @@
+
+// const a = parseFloat(arg)
+// const b = parseFloat(arg)
+
+const more = (a) => a + 1
+const less = (a) => a - 1
+const add = (a, b) => a + b
+const sub = (a, b) => b - a
+
+// function more(arg){
+//    const n = parseFloat(arg)
+//    return n
+// }
+
+// function less(arg){
+//    const n = parseFloat(arg)
+//    return n
+// }
+// function add(arg){
+//    const n = parseFloat(arg)
+//    return n
+// }
+
+// function sub(arg){
+//    const n = parseFloat(arg)
+//    return n
+// }
