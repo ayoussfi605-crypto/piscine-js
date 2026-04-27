9cc59725595a738f733c8753e94006f1cc9dab35
aelyoussef <ayoubyouss4859@gmail.com>
2026-04-27 14:51:27 +0100

    fix mutability 4

--- Data/mutability.js
+++ Data/mutability.js
@@ -1,12 +1,6 @@
-const person = {
-  name: 'Rick',
-  age: 77,
-  country: 'US',
-}
-
 const clone1 = {...person};
 const clone2 = {...person};
 const samePerson = person;
 
-person.age = person.age + 1;
+person.age++;
 person.country = 'FR';
