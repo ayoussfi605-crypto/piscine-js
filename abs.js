38fe4ce3fc6975ab6d8cc53129a860a24836047c
ayoussfi605-crypto <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>
2026-04-28 15:45:08 +0100

    abs

---
+++ abs.js
@@ -0,0 +1,11 @@
+
+// function isPositive(nbr){
+//     if (nbr > 0){
+//         return true
+//     }
+//     return false
+// }
+
+const isPositive = (value) => (value > 0 ? true : false);
+
+const abs = (value) => (value >= 0 ? value : -value)
