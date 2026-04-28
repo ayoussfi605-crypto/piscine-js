c9278f481e1d0167f91070376e018ec4c22532e4
ayoussfi605-crypto <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>
2026-04-28 16:37:44 +0100

    sign

---
+++ sign.js
@@ -0,0 +1,20 @@
+// function sign(nbr) {
+//     if (nbr == 0){
+//         return 0
+//     }else if (nbr > 0){
+//         return 1
+//     }
+// }
+
+// const sign = (nbr) => {
+//     if (nbr == 0){
+//         return 0
+//     }
+//     return  (nbr > 0 ? 1 : -1);
+// }
+
+const sign = nbr => nbr === 0 ? 0 : (nbr > 0 ? 1 : -1) 
+
+const sameSign = (a,b) => a === b ? true : false;
+
+
