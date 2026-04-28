d93f89822c5b81b6f921daafd1ee9eb861112e92
ayoussfi605-crypto <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>
2026-04-28 18:53:27 +0100

    typeof

---
+++ is.js
@@ -0,0 +1,22 @@
+
+is.num = (arg) =>  typeof arg == "number" 
+    
+is.nan = (arg) =>  typeof arg !== "number" 
+
+is.str = (arg) =>  typeof arg == "string" 
+
+is.bool = (arg) =>  typeof arg == "boolean" 
+
+is.undef = (arg) =>  typeof arg == "undefined" 
+
+is.def = (arg) =>  typeof arg !== "undefined"
+
+is.arr = (arg) =>  typeof arg == "object"
+
+is.obj = (arg) =>  typeof arg == "object"
+
+is.fun = (arg) =>  typeof arg == "function"
+
+is.truthy = (arg) =>  !!arg;
+
+is.falsy = (arg) =>  !!arg;
