1d95f10d58c1d138589cd0537e8f01ffc60e9694
ayoussfi605-crypto <ayoussfi605@gmail.com>
2026-04-30 00:53:57 +0100

    keep

---
+++ keep-cut.js
@@ -0,0 +1,65 @@
+let str = 'abcdef'
+
+function cutFirst(str){
+    let res = ""
+    for(let i = 2 ; i < str.length; i++){
+        res += str[i]
+    }
+    return res
+}
+
+function cutLast(str){
+    let res = ""
+    for(let i = 0 ; i < str.length-2; i++){
+        res += str[i]
+    }
+    return res
+}
+
+function cutFirstLast(str){
+    let res = ""
+    for(let i = 2 ; i < str.length-2; i++){
+        res += str[i]
+    }
+    return res
+}
+
+function keepFirst(str){
+    let res = ""
+    for(let i = 0 ; i < 2; i++){
+       
+            res += str[i]
+        
+        if (str.length === 1){
+            res = str 
+        }
+    }
+    return res
+}
+
+function keepLast(str){
+    let res = ""
+    for(let i = str.length-2 ; i < str.length; i++){
+
+        res += str[i]
+        
+         if (str.length === 1){
+            res = str 
+        }
+    }
+    return res
+}
+
+
+function keepFirstLast(str){
+    let res = ""
+    for(let i = 0 ; i < str.length; i++){
+        if (i > 1 && i < str.length-2){
+            continue
+        }
+        res += str[i]
+    }
+    return res
+}
+
+console.log(keepLast(str));
---
+++ slicer.js
@@ -0,0 +1,31 @@
+let arr = "ayoub"
+
+function slice(arr, start, end){
+
+    const type = typeof arr === "string"
+    if (end === undefined){
+        end = arr.length
+    }
+    if (start < 0){
+       start = arr.length + start
+   }
+    if (end < 0){
+        end = arr.length + end
+    }
+    
+    let slc = []
+    if (type){
+         slc = ""
+    }
+    for(let i = start ; i < end ; i++){
+        if (!type){
+            slc.push(arr[i])
+        }
+        if (type){
+
+            slc += arr[i]
+        }
+    }
+    return slc
+}
+console.log(slice(arr, 2))
