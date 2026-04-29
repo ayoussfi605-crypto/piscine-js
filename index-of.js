1038475b70b2f1c4601bd81f77eeeabc51a8707d
ayoussfi605-crypto <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>
2026-04-29 19:11:36 +0100

    indexof

--- index-of.js
+++ index-of.js
@@ -1,27 +1,82 @@
 
-function indexOf(arr, values){
-    for (let i = 0 ; i < arr.length ; i++){
-        if (arr[i] == values){
-            return i
-        }
-    }
-        return -1
-};
+// function indexOf(arr, values, indx){
 
-function lastIndexOf(arr, values){
-    for (let i = arr.length-1 ; i >= 0 ; i--){
-        if (arr[i] == values){
-            return i
+//     if (indx === undefined){
+//         indx = 0
+//     } 
+//     for (let i = indx ; i < arr.length ; i++){
+//         if (arr[i] === values){
+//             return i;
+//         }
+//     }
+//         return -1;
+// }
+
+// function lastIndexOf(arr, values, indx){
+//         if (indx === null){
+//         indx = 0
+//     }
+//     for (let i = arr.length-1 ; i >= indx ; i--){
+//         if (arr[i] == values){
+//             return i
+//         }
+//     }
+//         return -1
+// }
+
+// function includes(arr, values, indx){
+//         if (indx === null){
+//         indx = 0
+//     }
+//     for (let i = indx ; i < arr.length ; i++){
+//         if (arr[i] == values){
+//             return true
+//         }
+//     }
+//         return false
+// }
+
+// console.log(indexOf([1,2,3,3], 3, 2));
+
+
+function indexOf(arr, value, indx) {
+    if (indx === undefined) {
+        indx = 0;
+    }
+
+    for (let i = indx; i < arr.length; i++) {
+        if (arr[i] === value) {
+            return i;
         }
     }
-        return -1
+
+    return -1;
 }
 
-function includes(arr, values){
-    for (let i = 0 ; i < arr.length ; i++){
-        if (arr[i] == values){
-            return true
+function lastIndexOf(arr, value, indx) {
+    if (indx === undefined || indx === null) {
+        indx = arr.length - 1;
+    }
+
+    for (let i = indx; i >= 0; i--) {
+        if (arr[i] === value) {
+            return i;
         }
     }
-        return false
+
+    return -1;
+}
+
+function includes(arr, value, indx) {
+    if (indx === undefined) {
+        indx = 0;
+    }
+
+    for (let i = indx; i < arr.length; i++) {
+        if (arr[i] === value) {
+            return true;
+        }
+    }
+
+    return false;
 }
