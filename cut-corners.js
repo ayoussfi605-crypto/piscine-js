0f564ff1158b7569f51a2bb4f5ba487c92b1790e
ayoussfi605-crypto <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>
2026-04-30 17:08:53 +0100

    fk curnr

--- cut-corners.js
+++ cut-corners.js
@@ -1,80 +1,88 @@
+const getInt = (num) => {
+    let abs = num < 0 ? -num : num;
+    let res = 0;
+
+    let step = 1;
+
+    while (step <= abs) {
+        step *= 2;
+    }
+
+    step /= 2;
+
+    while (step >= 1) {
+        if (res + step <= abs) {
+            res += step;
+        }
+        step /= 2;
+    }
+
+    return num < 0 ? -res : res;
+};
+
+
 function round(nbr){
 
-   const int = modulo(nbr, 1)
+const int = getInt(nbr)
 
-const negative = (int) < 0 ? -int : int;
+let num = nbr - int 
 
-if(int < 0){
+const negative = (num) < 0 ? -num : num;
+
+if(num < 0){
 
     if ( negative <= 0.5){
     
-        return +(nbr - int)
+        return +(nbr - num)
     }
-    return +(nbr - int + (-1))
+    return +(nbr - num + (-1))
 }
     if ( negative <= 0.5){
     
-        return nbr - int
+        return nbr - num
     }
-    return nbr - int + 1
+    return nbr - num + 1
 
 return nbr
 }
 
+
 function floor(nbr){
 
-   const int = modulo(nbr, 1)
+   
+const int = getInt(nbr)
 
-   if (int !== 0){
+let num = nbr - int 
+
+   if (num !== 0){
     
-    if (nbr < 0 && int < 0){
-        return nbr - int -1
+    if (nbr < 0 && num < 0){
+        return nbr - num -1
        }
-       return (nbr - int) 
+       return (nbr - num) 
     }
 
     return nbr
 }
 
+
 function trunc(nbr){
-
-   const int = modulo(nbr, 1)
-
-   if (int !== 0){
-        return nbr - int 
-       }
-       return nbr 
+    return getInt(nbr) 
 }
+
 
 function ceil(nbr){
-
-   const int = modulo(nbr, 1)
-
-   if (int > 0){
-        return nbr - int + 1
-       }
-    if (int < 0){
-        return nbr - int 
-       }
-       return nbr 
+    
+    const int = getInt(nbr)
+    let i = nbr - int
+    
+    if (i > 0){
+        return nbr - i + 1
+    }
+    if (i < 0){
+        return nbr - i 
+    }
+    return nbr 
 }
 
 
-function modulo(a,b){
-
-    if (b == 0){
-        return
-    }
-
-    const negative = a < 0;
-
-    if (a < 0) a = -a;
-    if (b < 0) b = -b; 
-
-    let res = a ; 
-
-    while (res >= b){
-        res = res - b;
-    }   
-    return negative ? -res : res;
-};
