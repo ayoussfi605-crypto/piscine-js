8462c0a6a5e3b64b85466c18fd8fac81a1bb9f2d
aelyoussef <ayoubyouss4859@gmail.com>
2026-05-01 15:34:57 +0100

    triangle

---
+++ triangle.js
@@ -0,0 +1,17 @@
+
+
+function triangle(str, nbr){
+    let res = ""
+    for(let i = 0 ; i < nbr; i++){
+        let x = i
+        while(x >= 0){
+            res += str  
+            x--
+        }
+        if(i !== nbr-1){
+            res += "\n"
+        }
+    }
+    return res
+}
+
