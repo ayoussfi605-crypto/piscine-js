1e46e2da4f3915a119a70395d21e0a16a8f2ebdb
ayoussfi605-crypto <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>
2026-05-01 11:23:08 +0100

    split

--- unbreakable.js
+++ unbreakable.js
@@ -1,22 +1,47 @@
 
+let arr = ['ee', 'ff', 'g', '']
+
 
 function split(str, sep){
+
+    if(sep === ''){
+        let arr = []
+    for(let i = 0 ; i < str.length ; i++){
+                arr.push(str[i])
+            }
+            return arr
+    }
+
     let arr = []
     let res = ""
-    for(let i = 0 ; i < str.length ; i++){
-     
-        if(str[i] !== sep){
-        res += str[i]
+     while(str.length > 0){
+        let indx = str.indexOf(sep)
+        if(indx === -1){
+            arr.push(str)
+            break
         }
-        if(str[i] === sep && res !== ""){ 
+        res = str.slice(0, indx)
         arr.push(res)
-        res = ""
-    }
+        str = str.slice(indx + sep.length)
+     }
         
-    }
-    if(res !== ""){
-        arr.push(res)
-    }
-    return arr
+     if (str === ""){
+         arr.push("")
+        }
+        return arr
 }
 
+
+
+function join(arr, sep){
+    let res = ""
+    for(let i = 0 ; i < arr.length ; i++){
+        if(arr[i] === arr[arr.length-1]){
+            res += arr[i] 
+        }else{
+            res+= arr[i] +sep
+        }
+    }
+    return res
+}
+console.log(join(arr, ','));
