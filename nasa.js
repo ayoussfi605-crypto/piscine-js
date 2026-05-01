e0186b064e554288ccc98e8ce6b58075abcd3b55
ayoussfi605-crypto <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>
2026-05-01 12:11:59 +0100

    nasa

---
+++ nasa.js
@@ -0,0 +1,19 @@
+function nasa(nbr){
+
+    let res = ""
+    for(let i = 1 ; i <= nbr ; i++ ){
+        if(i % 3 === 0 && i % 5 === 0){
+            res+= "NASA" + " "
+        }else if(i % 3 === 0){
+            res+= "NA" + " "
+        }else if(i % 5 === 0){
+            res+= "SA" + " "
+        }else{
+            res += i + " "
+        }
+    }
+
+    
+    res = res.slice(0, res.length-1)
+    return res
+}
