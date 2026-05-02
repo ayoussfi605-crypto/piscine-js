9e991ae2c896af5d194dba639b75b3e5b2426eab
aelyoussef <ayoubyouss4859@gmail.com>
2026-05-02 11:50:30 +0100

    same-amt

--- same-amount.js
+++ same-amount.js
@@ -1,10 +1,13 @@
 
-function sameAmount(str , rgx1, rgx2){
-    let res = ""
-    res = str.match(rgx1).length === str.match(rgx2).length
-    console.log(str.match(rgx1).length);
+
+function sameAmount(str , rgx01 , rgx02){
+    let r1 = new RegExp(rgx01.source, "g")
+    let r2 = new RegExp(rgx02.source, "g")
+    let res = Boolean
+    res = str.match(r1)?.length === str.match(r2)?.length 
     if (str === null){
         return false
     }
     return res
 }
+
