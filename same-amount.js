e3c7885006e756563d38dabaca1c7278b8a925df
aelyoussef <ayoubyouss4859@gmail.com>
2026-05-02 10:49:25 +0100

    samz=e-amt

--- same-amount.js
+++ same-amount.js
@@ -1,6 +1,6 @@
 
 function sameAmount(str , rgx1, rgx2){
-
+    let res = ""
     res = str.match(rgx1).length === str.match(rgx2).length
     console.log(str.match(rgx1).length);
     if (str === null){
