67d7d37c4ef969e6edfcfb28dd70a37d64f09845
aelyoussef <ayoubyouss4859@gmail.com>
2026-05-02 10:44:58 +0100

    sameam

---
+++ same-amount_test.js
@@ -0,0 +1,11 @@
+
+function sameAmount(str , rgx1, rgx2){
+
+
+    res = str.match(rgx1).length === str.match(rgx2).length
+    console.log(str.match(rgx1).length);
+    if (str === null){
+        return false
+    }
+    return res
+}
