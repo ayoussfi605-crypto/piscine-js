1a5f6e33952b8d62ec3e1c01501d36b0237f534c
ayoussfi605-crypto <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>
2026-05-05 20:12:05 +0100

    date-is

---
+++ date-is.js
@@ -0,0 +1,22 @@
+function isValid(date){
+if(date instanceof Date || date === 'number') return !isNaN(date);
+}
+function isAfter(date1 , date2){
+    return date1 > date2 
+}
+function isBefore(date1 , date2){
+    return date1 < date2 
+}
+function isFuture(date){
+    let ftr = new Date
+    if(isValid(date) && date > ftr){
+        return true
+    }
+    return false
+}
+function isPast(date){
+    let Past = new Date
+    return isValid(date) && date < Past
+}
+
+                 
