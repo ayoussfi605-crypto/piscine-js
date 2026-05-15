89a5cd11e89f034b94cee6d997fac30d49fe74a6
aelyoussef <ayoubyouss4859@gmail.com>
2026-05-15 20:39:33 +0100

    throttle

--- throttle.js
+++ throttle.js
@@ -1,17 +1,37 @@
-function throttle(fn, wait){
-    let timeout;
-   return function(...arg){
-    //clearTimeout(timeout)
-    if (!timeout){
-        fn(...arg)
+function throttle(fn, delay){
+    let lastTime = 0
+    return function (...args){
+        let now = new Date()
+        if (now - lastTime >= delay){
+            lastTime = now
+            fn(...args)
+        }
     }
-     timeout = setTimeout(() =>
-         timeout = null
-     ,wait);
-    } 
 }
 
 
-// function opThrottle(fn, wait, option = { leading: true }){
+function opThrottle(fn, delay, options = {}) {
+    let lastTime = 0
+    let timerID = null
 
-// }
+    return function (...args) {
+    const now = Date.now()
+    if (options.leading && now - lastTime >= delay) {
+        lastTime = now
+        fn(...args)
+        return
+    }
+
+    if (options.trailing && !timerID) {
+        if (lastTime === 0) lastTime = now
+
+        const remaining = delay - (now - lastTime)
+
+        timerID = setTimeout(() => {
+        timerID = null
+        lastTime = Date.now()
+        fn(...args)
+        }, remaining > 0 ? remaining : 0)
+    }
+    }
+}
