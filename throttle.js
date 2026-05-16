62082046a4d359cf936fbe89662e7bdac74aceed
aelyoussef <ayoubyouss4859@gmail.com>
2026-05-16 10:41:56 +0100

    throttle

--- throttle.js
+++ throttle.js
@@ -1,37 +1,38 @@
-function throttle(fn, delay){
-    let lastTime = 0
-    return function (...args){
-        let now = new Date()
-        if (now - lastTime >= delay){
-            lastTime = now
-            fn(...args)
+
+function throttle(fn, wait) {
+    let timeout;
+    return function (...arg) {
+        if (!timeout) {
+            fn(...arg)
+        }
+        timeout = setTimeout(() =>
+
+            timeout = null
+            , wait)
+    }
+}
+
+
+
+
+function opThrottle(fn, wait, option = {}) {
+    let timeout;
+    return function(...arg){
+        if(option.leading){
+            if(!timeout){
+                fn(...arg)
+                timeout = setTimeout(() => 
+                    timeout = null 
+                , wait)
+
+            }
+        }else if(option.trailing){
+            if(timeout){
+                fn(...arg)
+            }
+            timeout = setTimeout(() => 
+                timeout = null 
+            , wait)
         }
     }
-}
-
-
-function opThrottle(fn, delay, options = {}) {
-    let lastTime = 0
-    let timerID = null
-
-    return function (...args) {
-    const now = Date.now()
-    if (options.leading && now - lastTime >= delay) {
-        lastTime = now
-        fn(...args)
-        return
-    }
-
-    if (options.trailing && !timerID) {
-        if (lastTime === 0) lastTime = now
-
-        const remaining = delay - (now - lastTime)
-
-        timerID = setTimeout(() => {
-        timerID = null
-        lastTime = Date.now()
-        fn(...args)
-        }, remaining > 0 ? remaining : 0)
-    }
-    }
-}
+} 
