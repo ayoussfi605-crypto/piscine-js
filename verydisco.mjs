411ee5adb94058df2df548ca2577eb6f782f68b2
ayoussfi605-crypto <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>
2026-05-18 16:18:06 +0100

    start learn node

--- verydisco.mjs
+++ verydisco.mjs
@@ -1,12 +1,18 @@
 const args = process.argv
-let s = args.slice(2);
- 
-    let result = [] ;
-    s.forEach(element => {
-        let x = Math.ceil(element.length / 2)
-        let y = element.slice(x, element.length) + element.slice(0, x)
-        result.push(y)
-    });
-    console.log(result.join(' '));
+let s = args.slice(2,3);
+let y = [];
+s.forEach(element => {
     
+    y = y.concat(element.split(' '))
+   
+});
+
+
+let result = [] ;
+y.forEach(element => {
+    let x = Math.ceil(element.length / 2)
+    let a = element.slice(x, element.length) + element.slice(0, x)
+    result.push(a)
+});
+console.log(result.join(' '));
 
